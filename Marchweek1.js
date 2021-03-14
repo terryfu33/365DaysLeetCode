5bca395d1670e32aa09f93f5d99054c3176e1afb
terryfu <terryfu33@gmail.com>
2021-03-14 17:47:01 -0600

    first commit

---
+++ Marchweek1.js
@@ -0,0 +1,22 @@
+//286.Missing Number
+var missingNumber = function(nums) {
+    
+    nums.sort(function(a, b){return a - b});
+    console.log(nums);
+    var i;
+    var size = nums.length;
+    var expected = 0;
+    for(i = 0;i<size;i++){
+        if(expected === nums[i]){
+            expected ++;
+            continue;
+        }
+        else{
+            return expected;
+        }
+    }
+    
+    return size;
+};
+
+console.log(missingNumber([45,35,38,13,12,23,48,15,44,21,43,26,6,37,1,19,22,3,11,32,4,16,28,49,29,36,33,8,9,39,46,17,41,7,2,5,27,20,40,34,30,25,47,0,31,42,24,10,14]))
