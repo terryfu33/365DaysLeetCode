3c23328570be6348df46dbb88c1f1e2ae7ed010e
terryfu <terryfu33@gmail.com>
2021-03-14 17:49:43 -0600

    asdf

--- Marchweek1.js
+++ Marchweek1.js
@@ -1,3 +1,15 @@
+//287.Find the Duplicate Number
+var findDuplicate = function(nums) {
+    
+};
+
+
+
+
+
+
+
+
 //286.Missing Number
 var missingNumber = function(nums) {
     
@@ -19,4 +31,3 @@ var missingNumber = function(nums) {
     return size;
 };
 
-console.log(missingNumber([45,35,38,13,12,23,48,15,44,21,43,26,6,37,1,19,22,3,11,32,4,16,28,49,29,36,33,8,9,39,46,17,41,7,2,5,27,20,40,34,30,25,47,0,31,42,24,10,14]))
